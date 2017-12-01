24b30091d99cd19968fba0e92f7e30b2cb4179ca
Greg Mertzlufft <gmertzlufft@marchex.com>
2017-11-30 23:25:24 -0800

    Remove garbage

--- src/main/java/org/hacksy/day01/Day01AProcessor.java
+++ src/main/java/org/hacksy/day01/Day01AProcessor.java
@@ -2,17 +2,13 @@ package org.hacksy.day01;
 
 public class Day01AProcessor {
     public int run(String input) {
-        if (input.length() > 0) {
-            int acc = 0;
-            for(int i = 0; i < input.length(); i++) {
-                int compareIndex = (i + 1) % (input.length());
-                if (input.charAt(i) == input.charAt(compareIndex)) {
-                    acc += Integer.parseInt(String.valueOf(input.charAt(i)));
-                }
+        int acc = 0;
+        for(int i = 0; i < input.length(); i++) {
+            int compareIndex = (i + 1) % (input.length());
+            if (input.charAt(i) == input.charAt(compareIndex)) {
+                acc += Integer.parseInt(String.valueOf(input.charAt(i)));
             }
-            return acc;
-        } else {
-            return 0;
         }
+        return acc;
     }
 }
--- src/main/java/org/hacksy/day01/Day01BProcessor.java
+++ src/main/java/org/hacksy/day01/Day01BProcessor.java
@@ -2,17 +2,13 @@ package org.hacksy.day01;
 
 public class Day01BProcessor {
     public int run(String input) {
-        if (input.length() > 0) {
-            int acc = 0;
-            for(int i = 0; i < input.length(); i++) {
-                int compareIndex = (i + input.length() / 2) % input.length();
-                if (input.charAt(i) == input.charAt(compareIndex)) {
-                    acc += Integer.parseInt(String.valueOf(input.charAt(i)));
-                }
+        int acc = 0;
+        for(int i = 0; i < input.length(); i++) {
+            int compareIndex = (i + input.length() / 2) % input.length();
+            if (input.charAt(i) == input.charAt(compareIndex)) {
+                acc += Integer.parseInt(String.valueOf(input.charAt(i)));
             }
-            return acc;
-        } else {
-            return 0;
         }
+        return acc;
     }
 }
